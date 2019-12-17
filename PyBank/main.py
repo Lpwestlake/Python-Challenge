ac1bf0194a53bac760d58cc7b2fb8dcc794e332a
Lpwestlake <57381750+Lpwestlake@users.noreply.github.com>
2019-12-16 21:17:05 -0600

    corrected relative path.  Need to run file from each specific directory (pybank/pypoll)

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,14 @@
-import csv
 import os
+import csv 
 
-budget_path = os.path.join("Resources\budget_data.csv")
+budget_path = os.path.join("Resources/budget_data.csv")
+
 
 # C:\Users\westl\Desktop\Python-Challenge\Python-Challenge\PyBank\Resources\budget_data.csv
 
 with open(budget_path, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
-    print("hello")
+    for row in csv_reader:
+        print(row)
     
